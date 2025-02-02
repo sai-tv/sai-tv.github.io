ffd78696f0c223684c3efa91419bda123fcbb5de
sai-tv <118874434+sai-tv@users.noreply.github.com>
2025-02-02 07:26:17 +0000

    Deploying to gh-pages from @ sai-tv/sai-tv.github.io@4f2259d3463ea3f13ab0e683edd7f4e746178d15 🚀

---
+++ svelte-reactive-map-list-master/src/components/stores.js
@@ -0,0 +1,1 @@
+import{writable}from"svelte/store";export const activeListItem=writable(0);export const activeMapItem=writable(0);
---
+++ svelte-reactive-map-list-master/src/main.js
@@ -0,0 +1,1 @@
+import App from"./components/App.svelte";const app=new App({target:document.body});export default app;
