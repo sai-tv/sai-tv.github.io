4f2259d3463ea3f13ab0e683edd7f4e746178d15
sai-tv <saicharantv@hotmail.com>
2025-02-02 07:21:21 +0000

    add reactive-svelte-map

---
+++ _pages/map.md
@@ -0,0 +1,9 @@
+---
+layout: default
+title: map
+permalink: /map/
+nav: true
+nav_order: 9
+---
+
+ <iframe src="/assets/svelte-reactive-map-list/index.html" style="width:100%; height:600px; border:none;"></iframe>
---
+++ svelte-reactive-map-list-master/src/components/consts.js
@@ -0,0 +1,43 @@
+export const sprudgeArticle = "https://sprudge.com/the-sprudge-guide-to-coffee-in-kyoto-128389.html";
+
+export const accessToken = "pk.eyJ1IjoiYnVyY2UiLCJhIjoiY2s0M2huOWhkMDgydDNrcnF6NGZhYnJlZSJ9._HWRFgau8GQulfNwSzyIlQ";
+
+export const listItems = [
+  {
+    name: "Arsicault Bakery",
+    address: "397 Arguello Blvd, San Francisco, CA 94118",
+    website: "https://arsicault-bakery.com/",
+    image: "https://richmondsfblog.com/wp-content/uploads/2015/04/arsicault4.jpg",
+    description:
+      "<p>Smaller offering, but can’t go wrong with the almond. The other little knick-knacks are great too (financiers are decadent and great to share, and so are the shortbreads). If you go on Sundays, the Clement Street Farmers’ Market is great to hit up as well.</p>",
+    coordinates: [-122.459227, 37.7834067],
+  },
+  {
+    name: "Day Moon",
+    description:
+      "<p>My absolute favorite bakery. Period. Their scone ingredients are always surprising but they never miss. The baguette sandwiches on weekends are maybe the best in the area. Their iced mochas are a must try as well (they never give me their recipe).</p>",
+    address: "3928 Irving St, San Francisco, CA 94122",
+    image: "https://res.cloudinary.com/the-infatuation/image/upload/q_auto,f_auto/images/MZink-SF-DayMoon-Exterior-05_fzreu9",
+    website: "https://www.instagram.com/daymoonbread/?hl=en",
+    coordinates: [-122.50016784667969, 37.762691497802734],
+  },
+  {
+    name: "Juniper",
+    description:
+      "<p>Great new bakery. They’ve even won awards for the best croissant: super crunchy on the outside. Their cubano croissant is something I could eat everyday, and their mochas are great too. Great spot, but parking could be a slight issue here.</p>",
+    address: "1401 Polk St, San Francisco, CA 94109",
+    image: "https://images.squarespace-cdn.com/content/v1/63acc3a1b081be6926a772e2/9fe741bf-5eba-4c9b-880c-2952855faa53/Juniper+Front.jpg",
+    website: "http://www.juniper.cafe/",
+    coordinates: [-122.420689, 37.7897377],
+  },
+  {
+    name: "Tartine",
+    description:
+      "<p>Out of all of the Tartine locations, I think my favorite has to be outer sunset. Bread pudding is fantastic, and the location is great. Hop over to Golden Gate park to feed the ducks around Stow Lake. They love the bread pudding too.</p>",
+    address: "1226 9th Ave, San Francisco, CA 94122",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/GYGnZUvdwoPrJNqi-axjV3lN2wo=/0x0:2000x1333/1200x0/filters:focal(0x0:2000x1333):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/18309923/TartineSunset_PChang_3835.jpg",
+    website: "https://tartinebakery.com/san-francisco/inner-sunset",
+    coordinates: [-122.468187, 37.760216],
+  },
+];
